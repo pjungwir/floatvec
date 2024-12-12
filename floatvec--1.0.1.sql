31ea4eb1deadf2560d73bec7db112e8c4a369e51
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2024-12-11 16:06:43 -0800

    Fix version number in sql file

--- floatvec--1.0.1.sql
+++ floatvec--1.0.1.sql
@@ -1,4 +1,4 @@
-/* floatvec--1.0.sql */
+/* floatvec--1.0.1.sql */
 
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION floatvec" to load this file. \quit

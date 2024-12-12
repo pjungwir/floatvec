fedb308be9374e23e63dfef197f8ee5627d11ea1
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2024-12-11 16:10:06 -0800

    Version bump to 1.1.1

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 MODULES = floatvec
 EXTENSION = floatvec
-EXTENSION_VERSION = 1.1.0
+EXTENSION_VERSION = 1.1.1
 DATA = floatvec--$(EXTENSION_VERSION).sql
 
 REGRESS = setup \
---
+++ floatvec--1.1.1.sql
@@ -0,0 +1,113 @@
+/* floatvec--1.1.1.sql */
+
+-- complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use "CREATE EXTENSION floatvec" to load this file. \quit
+
+
+-- add
+
+CREATE OR REPLACE FUNCTION
+vec_add(anyarray, anyarray)
+RETURNS anyarray
+AS 'floatvec', 'vec_add_with_vec'
+LANGUAGE c;
+
+CREATE OR REPLACE FUNCTION
+vec_add(anyarray, anyelement)
+RETURNS anyarray
+AS 'floatvec', 'vec_add_with_scalar'
+LANGUAGE c;
+
+CREATE OR REPLACE FUNCTION
+vec_add(anyelement, anyarray)
+RETURNS anyarray
+AS 'floatvec', 'vec_add_with_scalar'
+LANGUAGE c;
+
+-- sub
+
+CREATE OR REPLACE FUNCTION
+vec_sub(anyarray, anyarray)
+RETURNS anyarray
+AS 'floatvec', 'vec_sub_with_vec'
+LANGUAGE c;
+
+CREATE OR REPLACE FUNCTION
+vec_sub(anyarray, anyelement)
+RETURNS anyarray
+AS 'floatvec', 'vec_sub_with_scalar'
+LANGUAGE c;
+
+CREATE OR REPLACE FUNCTION
+vec_sub(anyelement, anyarray)
+RETURNS anyarray
+AS 'floatvec', 'vec_sub_with_scalar'
+LANGUAGE c;
+
+-- mul
+
+CREATE OR REPLACE FUNCTION
+vec_mul(anyarray, anyarray)
+RETURNS anyarray
+AS 'floatvec', 'vec_mul_with_vec'
+LANGUAGE c;
+
+CREATE OR REPLACE FUNCTION
+vec_mul(anyarray, anyelement)
+RETURNS anyarray
+AS 'floatvec', 'vec_mul_with_scalar'
+LANGUAGE c;
+
+CREATE OR REPLACE FUNCTION
+vec_mul(anyelement, anyarray)
+RETURNS anyarray
+AS 'floatvec', 'vec_mul_with_scalar'
+LANGUAGE c;
+
+-- div
+
+CREATE OR REPLACE FUNCTION
+vec_div(anyarray, anyarray)
+RETURNS anyarray
+AS 'floatvec', 'vec_div_with_vec'
+LANGUAGE c;
+
+CREATE OR REPLACE FUNCTION
+vec_div(anyarray, anyelement)
+RETURNS anyarray
+AS 'floatvec', 'vec_div_with_scalar'
+LANGUAGE c;
+
+CREATE OR REPLACE FUNCTION
+vec_div(anyelement, anyarray)
+RETURNS anyarray
+AS 'floatvec', 'vec_div_with_scalar'
+LANGUAGE c;
+
+-- pow
+
+CREATE OR REPLACE FUNCTION
+vec_pow(anyarray, anyarray)
+RETURNS anyarray
+AS 'floatvec', 'vec_pow_with_vec'
+LANGUAGE c;
+
+CREATE OR REPLACE FUNCTION
+vec_pow(anyarray, anyelement)
+RETURNS anyarray
+AS 'floatvec', 'vec_pow_with_scalar'
+LANGUAGE c;
+
+CREATE OR REPLACE FUNCTION
+vec_pow(anyelement, anyarray)
+RETURNS anyarray
+AS 'floatvec', 'vec_pow_with_scalar'
+LANGUAGE c;
+
+-- elements
+
+CREATE OR REPLACE FUNCTION
+vec_elements(anyarray, integer[])
+RETURNS anyarray
+AS 'floatvec', 'vec_elements_from_int'
+LANGUAGE c;
